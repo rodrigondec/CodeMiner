e2fff19b0ed6d1fd374c82bb1124873fe1ff9f30
rodrigondec <rodrigondec@gmail.com>
2019-07-02 18:55:27 -0300

    attr_reader mean on Kill

--- kill.rb
+++ kill.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
+# Class representing a kill. Contains the killer, victim and the mean
 class Kill
   def initialize(killer, victim, mean)
     @killer = killer
     @victim = victim
     @mean = mean
   end
+
+  attr_reader :mean
 end
