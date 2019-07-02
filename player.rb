95aa506b39bc57e4d143cc5887a9fa9165491e06
rodrigondec <rodrigondec@gmail.com>
2019-07-01 21:31:06 -0300

    created Player class

---
+++ player.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Class representing a player. Contains the name and kill counter of the player
+class Player
+  def initialize(name)
+    @name = name
+    @kills = 0
+  end
+
+  attr_reader :name
+  attr_reader :kills
+
+  def increment_kill
+    @kills += 1
+  end
+
+  def decrement_kill
+    @kills -= 1
+  end
+end
