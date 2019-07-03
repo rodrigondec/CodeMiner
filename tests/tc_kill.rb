de734dd643cbdf166c09cff209993251aa0cb452
rodrigondec <rodrigondec@gmail.com>
2019-07-03 18:16:01 -0300

    TestKill with TypeCheckers

--- tests/tc_kill.rb
+++ tests/tc_kill.rb
@@ -16,5 +16,29 @@ class TestKill < Test::Unit::TestCase
     assert_equal(victim, kill.victim)
   end
 
+  def test_killer_nil
+    killer = nil
+    victim = Player.new('victim')
+    kill = Kill.new(killer, victim, 'test')
+    assert_instance_of(Kill, kill)
+  end
+
+  def test_killer_typecheck
+    killer = 'killer'
+    victim = Player.new('victim')
+    assert_raise(RuntimeError) { Kill.new(killer, victim, 'test') }
+  end
+
+  def test_victim_typecheck
+    killer = nil
+    victim = 'victim'
+    assert_raise(RuntimeError) { Kill.new(killer, victim, 'test') }
+  end
+
+  def test_mean_typecheck
+    killer = Player.new('killer')
+    victim = Player.new('victim')
+    assert_raise(RuntimeError) { Kill.new(killer, victim, 1) }
+  end
 
 end
