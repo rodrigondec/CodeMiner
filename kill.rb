b6063da6a3da5cfd20119707727e3d67f3a01ad1
rodrigondec <rodrigondec@gmail.com>
2019-07-03 18:16:48 -0300

    Kill class with guard clause for parameters

--- kill.rb
+++ kill.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
+require_relative 'player'
+
 # Class representing a kill. Contains the killer, victim and the mean
 class Kill
   def initialize(killer, victim, mean)
+    raise('Killer must be a Player instance or nil') unless
+      killer.is_a?(Player) || killer.nil?
+    raise('Victim must be a Player instance') unless victim.is_a?(Player)
+    raise('Mean must be a String') unless mean.is_a?(String)
+
     @killer = killer
     @victim = victim
     @mean = mean
   end
 
-  attr_reader :mean
+  attr_reader :mean, :killer, :victim
 end
