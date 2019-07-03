2126438cf32e4ec3b7f22ef0f4b482edd526c40f
rodrigondec <rodrigondec@gmail.com>
2019-07-03 18:52:07 -0300

    parser with helper methods

--- parser.rb
+++ parser.rb
@@ -19,19 +19,32 @@ class LogParser
 
   attr_reader :games
 
-  def info
-    @games.each do |game|
-      pp game.info
+  def get_game(game_index)
+    @games[game_index]
+  end
+
+  def get_game_info(game_index)
+    game = get_game(game_index)
+    game.info
+  end
+
+  def all_games_info
+    info = []
+    @games.each_index do |index|
+      info.push(get_game_info(index))
     end
+    info
   end
 
   private
 
   def parse
     game = nil
+    game_number = 1
     @data.each do |line|
       if line.include?('InitGame:')
-        game = Game.new(line)
+        game = Game.new("Game #{game_number}")
+        game_number += 1
         @games.push(game)
       elsif line.include?('ClientUserinfoChanged:')
         game.process_user_info_line(line)
