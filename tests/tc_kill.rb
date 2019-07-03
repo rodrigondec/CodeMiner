e730fb9ec18abc68ecdcfd6cc987265bb224c8a1
rodrigondec <rodrigondec@gmail.com>
2019-07-03 18:15:18 -0300

    def creation on TestKill

--- tests/tc_kill.rb
+++ tests/tc_kill.rb
@@ -6,6 +6,15 @@ require 'test/unit'
 
 class TestKill < Test::Unit::TestCase
 
-  
+  def test_creation
+    killer = Player.new('killer')
+    victim = Player.new('victim')
+    kill = Kill.new(killer, victim, 'test')
+    assert_instance_of(Kill, kill)
+    assert_equal('test', kill.mean)
+    assert_equal(killer, kill.killer)
+    assert_equal(victim, kill.victim)
+  end
+
 
 end
