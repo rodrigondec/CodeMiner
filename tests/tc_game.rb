29053b50e1e2f6ba3d0e5903c513cc0f261c1db6
rodrigondec <rodrigondec@gmail.com>
2019-07-03 18:14:36 -0300

    TestGame created

---
+++ tests/tc_game.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../kill'
+require_relative '../player'
+require_relative '../game'
+require 'test/unit'
+
+class TestGame < Test::Unit::TestCase
+
+  
+
+end
